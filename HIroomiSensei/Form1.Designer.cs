b1d10a72d9363d921b816ba161ec8b6dae601650
crlotwhite <crlotwhite@gmail.com>
2021-01-28 17:33:34 +0900

    초기 커밋.

---
+++ HIroomiSensei/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace HIroomiSensei
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbLang = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtH = new System.Windows.Forms.TextBox();
+            this.txtW = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbWindowsList = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtResult.Location = new System.Drawing.Point(12, 383);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(880, 156);
+            this.txtResult.TabIndex = 0;
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Font = new System.Drawing.Font("나눔고딕", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Result.Location = new System.Drawing.Point(8, 359);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(64, 21);
+            this.Result.TabIndex = 1;
+            this.Result.Text = "Result";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔고딕", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Source";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtSource.Location = new System.Drawing.Point(12, 200);
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(880, 156);
+            this.txtSource.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.cbLang);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtH);
+            this.groupBox1.Controls.Add(this.txtW);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtY);
+            this.groupBox1.Controls.Add(this.txtX);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cbWindowsList);
+            this.groupBox1.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(880, 161);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Windows Information";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(534, 80);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(197, 28);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Game Language";
+            // 
+            // cbLang
+            // 
+            this.cbLang.FormattingEnabled = true;
+            this.cbLang.Items.AddRange(new object[] {
+            "eng",
+            "jpn",
+            "kor"});
+            this.cbLang.Location = new System.Drawing.Point(599, 111);
+            this.cbLang.Name = "cbLang";
+            this.cbLang.Size = new System.Drawing.Size(75, 36);
+            this.cbLang.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(737, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 82);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Translate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtH
+            // 
+            this.txtH.Location = new System.Drawing.Point(189, 117);
+            this.txtH.Name = "txtH";
+            this.txtH.ReadOnly = true;
+            this.txtH.Size = new System.Drawing.Size(100, 35);
+            this.txtH.TabIndex = 9;
+            // 
+            // txtW
+            // 
+            this.txtW.Location = new System.Drawing.Point(189, 73);
+            this.txtW.Name = "txtW";
+            this.txtW.ReadOnly = true;
+            this.txtW.Size = new System.Drawing.Size(100, 35);
+            this.txtW.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(155, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 28);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "H";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(155, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 28);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "W";
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(45, 117);
+            this.txtY.Name = "txtY";
+            this.txtY.ReadOnly = true;
+            this.txtY.Size = new System.Drawing.Size(100, 35);
+            this.txtY.TabIndex = 5;
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(45, 73);
+            this.txtX.Name = "txtX";
+            this.txtX.ReadOnly = true;
+            this.txtX.Size = new System.Drawing.Size(100, 35);
+            this.txtX.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 28);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Windows";
+            // 
+            // cbWindowsList
+            // 
+            this.cbWindowsList.FormattingEnabled = true;
+            this.cbWindowsList.Location = new System.Drawing.Point(130, 31);
+            this.cbWindowsList.Name = "cbWindowsList";
+            this.cbWindowsList.Size = new System.Drawing.Size(744, 36);
+            this.cbWindowsList.TabIndex = 0;
+            this.cbWindowsList.DropDown += new System.EventHandler(this.cbWindowsList_DropDown);
+            this.cbWindowsList.SelectedIndexChanged += new System.EventHandler(this.cbWindowsList_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 551);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.txtResult);
+            this.Name = "Form1";
+            this.Text = "Oshiete Hiroomi Sensei";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtH;
+        private System.Windows.Forms.TextBox txtW;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbWindowsList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbLang;
+    }
+}
+
---
+++ HIroomiSensei/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using Tesseract;
+using OpenCvSharp;
+using Google.Cloud.Translation.V2;
+using Google.Apis.Auth.OAuth2;
+
+namespace HIroomiSensei
+{
+    
+    public struct Rect
+    {
+        public int Left { get; set; }
+        public int Top { get; set; }
+        public int Right { get; set; }
+        public int Bottom { get; set; }
+
+    }
+
+
+    public partial class Form1 : Form
+    {
+        private readonly string imgTemp = "temp.png";
+        private readonly string[] LangCodeForGoogle = { "en", "ja", "ko" };
+        private readonly string[] lstExcept = { "Runtime Broker", "svchost", "conhost" };
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cbWindowsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cbWindowsList.SelectedIndex >= 0)
+            {
+                // 프로세스 이름 가져오기
+                string s = cbWindowsList.SelectedItem as string;
+
+                // 이름 기반으로 프로세스 찾기
+                Process[] lstProc = Process.GetProcessesByName(s);
+
+                // 항상 유일한 프로세스 일것이라고 가정
+                Process gameProc = lstProc[0]; 
+
+                IntPtr ipMainWindow = gameProc.MainWindowHandle;
+                Rect gameRect = new Rect();
+                GetWindowRect(ipMainWindow, ref gameRect);
+
+                txtX.Text = gameRect.Left.ToString();
+                txtY.Text = gameRect.Top.ToString();
+                txtH.Text = (gameRect.Bottom - gameRect.Top).ToString();
+                txtW.Text = (gameRect.Right - gameRect.Left).ToString();
+
+
+            }
+        }
+
+        private void cbWindowsList_DropDown(object sender, System.EventArgs e)
+        {
+            // 프로세스 목록 업데이트
+            Process[] allProc = Process.GetProcesses();
+            foreach (Process p in allProc)
+            {
+                if (Array.IndexOf(lstExcept, p.ProcessName) < 0)
+                {
+                    cbWindowsList.Items.Add(p.ProcessName);
+                }
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 사실 상 메인 루틴
+            if (cbWindowsList.SelectedIndex >= 0 && cbLang.SelectedIndex >= 0)
+            {
+                
+                CaptureImage();
+                ImgPreprocess();
+                GetTextFromBMP();
+                TranslateText();
+
+                
+                if (File.Exists(imgTemp))
+                {
+                    File.Delete(imgTemp);
+                }
+
+            }
+                
+        }
+
+        public void CaptureImage()
+
+        {
+
+            try
+            {
+                // txtbox에 있는 값 가져오기
+                int x = Int32.Parse(txtX.Text);
+                int y = Int32.Parse(txtY.Text);
+                int w = Int32.Parse(txtW.Text);
+                int h = Int32.Parse(txtH.Text);
+
+                Rectangle rect = new Rectangle(x, y, w, h);
+
+                // 픽셀 포맷 정보 얻기 (Optional)
+                int bitsPerPixel = Screen.PrimaryScreen.BitsPerPixel;
+                PixelFormat pixelFormat = PixelFormat.Format32bppArgb;
+                if (bitsPerPixel <= 16)
+                {
+                    pixelFormat = PixelFormat.Format16bppRgb565;
+                }
+                if (bitsPerPixel == 24)
+                {
+                    pixelFormat = PixelFormat.Format24bppRgb;
+                }
+
+                // 화면 크기만큼의 Bitmap 생성
+                Bitmap bmp = new Bitmap(rect.Width, rect.Height, pixelFormat);
+
+                // Bitmap 이미지 변경을 위해 Graphics 객체 생성
+                using (Graphics gr = Graphics.FromImage(bmp))
+                {
+                    // 화면을 그대로 카피해서 Bitmap 메모리에 저장
+                    gr.CopyFromScreen(rect.Left, rect.Top, 0, 0, rect.Size);
+                }
+
+                
+
+
+                // Bitmap 데이타를 파일로 저장
+                bmp.Save(imgTemp, System.Drawing.Imaging.ImageFormat.Png);
+
+                // 메모리 해제
+                bmp.Dispose();
+
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show($"{e.GetType()}\n{e.Message}", "CaptureImage");
+            }
+
+        }
+
+        public void ImgPreprocess()
+        {
+            // 이미지 객체 생성
+            var img = Cv2.ImRead(imgTemp);
+            var dst = new Mat(img.Size(), MatType.CV_8UC1);
+
+            // temp.png 수정
+            Cv2.CvtColor(img, dst, ColorConversionCodes.BGR2GRAY);
+            Cv2.ImWrite(imgTemp, dst);
+
+            // 테스트용 출력
+            // Cv2.ImShow("Image", dst);
+
+            img.Dispose();
+            dst.Dispose();
+        }
+
+
+        public void GetTextFromBMP()
+        {
+            try 
+            {
+                // 이미지 가져오기
+                Bitmap img = new Bitmap(imgTemp);
+
+                // 언어 설정 가져오기
+                string lang = cbLang.SelectedItem as string;
+
+                // OCR엔진 생성
+                var ocr = new TesseractEngine($"tessdata/{lang}", lang, EngineMode.TesseractAndLstm);
+
+                // OCR처리
+                var texts = ocr.Process(img);
+
+                // 출력
+                //MessageBox.Show(texts.GetText());
+                string str_for_replace = texts.GetText();
+                str_for_replace.Replace("\n\n", "");
+                txtSource.Text = str_for_replace;
+
+                //메모리 해제
+                img.Dispose();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"{e.GetType()}\n{e.Message}", "GetTextFromBMP");
+            }
+
+        }
+
+        public void TranslateText()
+        {
+            int lang = cbLang.SelectedIndex;
+            string srcText = txtSource.Text;
+
+            var credential = GoogleCredential.FromFile("HiroomiSensei.json");
+            TranslationClient client = TranslationClient.Create(credential);
+            var response = client.TranslateText(text: srcText, targetLanguage: LangCodeForGoogle[2], sourceLanguage: LangCodeForGoogle[lang]);
+            txtResult.Text = response.TranslatedText;
+        }
+    }
+}
